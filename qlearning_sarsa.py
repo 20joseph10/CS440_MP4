911f46a8ab20ff17f6cd22664dad0dc62e3bafe9
Huiyun Wu <hwu63@illinois.edu>
2018-05-01 00:14:05 -0500

    Update qlearning_sarsa.py

--- qlearning_sarsa.py
+++ qlearning_sarsa.py
@@ -1,35 +1,36 @@
 import random
 
-ALPHA = 50
+C = 50
 GAMMA = 0.7
-EPSILON = 0.05
+EPSILON = 0.01
 
 
 # https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/blob/master/contents/3_Sarsa_maze/RL_brain.py
+# https://github.com/studywolf/blog/blob/master/RL/Cat%20vs%20Mouse%20exploration/qlearn.py
 class RL(object):
-    def __init__(self, alpha=ALPHA, gamma=GAMMA, epsilon=EPSILON):
+    def __init__(self):
         self.actions = [-1, 0, 1]  # a list
-        self.alpha = alpha
-        self.gamma = gamma
-        self.epsilon = epsilon
+        self.c = C
+        self.gamma = GAMMA
+        self.epsilon = EPSILON
 
-        self.q_table ={}
-        self.seen = {}
+        self.q_table = {}
+        self.N = {}
 
     def get_q(self, state, action):
         return self.q_table.get((state, action), 0.0)
 
     def learn_q(self, state, action, reward, value):
-        if (state, action) not in self.seen:
-            self.seen[(state, action)] = 0
-        self.seen[(state, action)] += 1
+        if (state, action) not in self.N:
+            self.N[(state, action)] = 0
+        self.N[(state, action)] += 1
         old_value = self.q_table.get((state, action), None)
         if old_value is None:
             self.q_table[(state, action)] = reward
         else:
             # C / (C + N(s, a))
-            self.q_table[(state, action)] = old_value + float(self.alpha) / float(
-                self.alpha + self.seen[(state, action)]) * (value - old_value)
+            self.q_table[(state, action)] = old_value + float(self.c) / float(
+                self.c + self.N[(state, action)]) * (value - old_value)
 
     def choose_action(self, state):
         if random.random() < self.epsilon:
@@ -47,27 +48,13 @@ class RL(object):
     def choose_action_random(self):
             return random.choice([-1.0, 0.0, 1.0])
 
-    def learn(self, *args):
-        pass
-
-
-# off-policy
-class QLearningTable(RL):
-    def __init__(self, alpha=ALPHA, gamma=GAMMA, epsilon=EPSILON):
-        super(QLearningTable, self).__init__(alpha, gamma, epsilon)
-
-    def learn(self, state1, action1, reward, state2):
+    def learn_qlearning(self, state1, action1, reward, state2):
         max_q_new = max([self.get_q(state2, a) for a in self.actions])
         self.learn_q(state1, action1, reward, reward + self.gamma * max_q_new)
 
-
-# on-policy
-class SarsaTable(RL):
-
-    def __init__(self, alpha=ALPHA, gamma=GAMMA, epsilon=EPSILON):
-        super(SarsaTable, self).__init__(alpha, gamma, epsilon)
-
-    def learn(self, state1, action1, reward, state2, action2):
+    def learn_sarsa(self, state1, action1, reward, state2, action2):
         q_next = self.get_q(state2, action2)
         self.learn_q(state1, action1, reward, reward + self.gamma * q_next)
+
+
 
